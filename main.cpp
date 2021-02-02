12dd3fb1b6deff6008af9b006fcec2ca742ce2ec
Limem Slim <slimlimem@gmail.com>
2021-02-02 23:35:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,15 @@ int main() {
     myQueue.push(std::chrono::milliseconds(2000));
     myQueue.push(std::chrono::milliseconds(3000));
 
+    std::chrono::milliseconds next(0);
     while (!myQueue.empty()) {
-        const auto& top = myQueue.top();
-        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(top).count() << std::endl;
+
+        const auto top = myQueue.top();
         myQueue.pop();
+        next = myQueue.top();
+        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(top).count() << std::endl;
+        std::this_thread::sleep_for(top);
+        std::cout << "----\n";
     }
 
     return 0;
